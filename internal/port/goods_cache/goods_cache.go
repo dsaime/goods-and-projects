d8599dd4e5fb2048dffdbb972dd7aaf3cbf7b4cf
Saime <acc.saime.d@gmail.com>
2025-06-17 17:53:02 +0300

    добавить коммент к кэшу

--- internal/port/goods_cache/goods_cache.go
+++ internal/port/goods_cache/goods_cache.go
@@ -4,13 +4,21 @@ import (
 	"github.com/dsaime/goods-and-projects/internal/domain"
 )
 
-// GoodsCache взаимодействует с кэшем, читать сохранить
+// GoodsCache представляет собой интерфейс кэша товаров
 type GoodsCache interface {
+	// Get Возвращает товар, сохраненный по переданному ключу.
+	// Если такой записи нет, вторым значением вернется false
 	Get(Key) (domain.Good, bool)
+
+	// Save Сохраняет товары в кэш.
+	// Каждому товару будет соответствовать свой ключ
 	Save(goods ...domain.Good)
+
+	// Delete Удаляет(инвалидирует) запись по ключу
 	Delete(...Key)
 }
 
+// Key представляет собой составной ключ товара
 type Key interface {
 	GetID() int
 	GetProjectID() int
